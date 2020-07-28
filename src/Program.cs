0d5704a54e599f987661edb62106f913e9a80f4e
Chester Lau <chesterlau@live.com>
2020-07-28 17:19:21 +1000

    Upgraded to .net core 3.1 and added Serilog

--- src/Program.cs
+++ src/Program.cs
@@ -1,24 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System.IO;
 
 namespace RefactorThis
 {
-    public class Program
+	public class Program
     {
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+		public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseKestrel()
+						.UseUrls("http://*:5000")
+						.UseContentRoot(Directory.GetCurrentDirectory())
+						.ConfigureAppConfiguration((hostingContext, config) =>
+						{
+							var env = hostingContext.HostingEnvironment;
+							config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+								  .AddJsonFile($"appsettings.{env.EnvironmentName}.json",
+									  optional: true, reloadOnChange: true);
+						})
+						.UseSerilog((ctx, config) =>
+						{
+							config.ReadFrom.Configuration(ctx.Configuration);
+							config.MinimumLevel.Override("Microsoft", LogEventLevel.Information);
+							config.MinimumLevel.Override("System", LogEventLevel.Warning);
+						}).UseStartup<Startup>();
+				});
+	}
 }
