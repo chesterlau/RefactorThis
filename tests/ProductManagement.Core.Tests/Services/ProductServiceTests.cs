76111369c6a96ab9315b36a61cd8b1949661d03b
Chester Lau <chesterlau@live.com>
2020-07-29 22:06:57 +1000

    Added product management core tests project

---
+++ tests/ProductManagement.Core.Tests/Services/ProductServiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ProductManagement.Core.Tests.Services
+{
+    public class ProductServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
