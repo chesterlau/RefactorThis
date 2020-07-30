6cfec72a4e47ce42a508fe58e262b9ede64103f8
Chester Lau <chesterlau@live.com>
2020-07-30 22:28:08 +1000

    Formatted tabs to spaces

--- src/ProductManagement.API/Middleware/LogContextMiddleware.cs
+++ src/ProductManagement.API/Middleware/LogContextMiddleware.cs
@@ -4,26 +4,26 @@ using System.Threading.Tasks;
 
 namespace ProductManagement.API.Middleware
 {
-	public class LogContextMiddleware
+    public class LogContextMiddleware
     {
-		private readonly RequestDelegate _next;
-		private const string _userAgentHeaderKey = "User-Agent";
-		private const string _correlationIdHeaderKey = "X-Correlation-ID";
+        private readonly RequestDelegate _next;
+        private const string _userAgentHeaderKey = "User-Agent";
+        private const string _correlationIdHeaderKey = "X-Correlation-ID";
 
-		public LogContextMiddleware(RequestDelegate next)
-		{
-			_next = next;
-		}
+        public LogContextMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
 
-		public async Task InvokeAsync(HttpContext context)
-		{
-			string userAgentFromHeader = context.Request.Headers.ContainsKey(_userAgentHeaderKey) ? context.Request.Headers[_userAgentHeaderKey].ToString() : null;
-			string correlationIdFromHeader = context.Request.Headers.ContainsKey(_correlationIdHeaderKey) ? context.Request.Headers[_correlationIdHeaderKey].ToString() : null;
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string userAgentFromHeader = context.Request.Headers.ContainsKey(_userAgentHeaderKey) ? context.Request.Headers[_userAgentHeaderKey].ToString() : null;
+            string correlationIdFromHeader = context.Request.Headers.ContainsKey(_correlationIdHeaderKey) ? context.Request.Headers[_correlationIdHeaderKey].ToString() : null;
 
-			LogContext.PushProperty(_userAgentHeaderKey, userAgentFromHeader);
-			LogContext.PushProperty(_correlationIdHeaderKey, correlationIdFromHeader);
+            LogContext.PushProperty(_userAgentHeaderKey, userAgentFromHeader);
+            LogContext.PushProperty(_correlationIdHeaderKey, correlationIdFromHeader);
 
-			await _next(context);
-		}
-	}
+            await _next(context);
+        }
+    }
 }
