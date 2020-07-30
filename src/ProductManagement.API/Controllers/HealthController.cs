60e26fb5df2de8acd2d3941857fcd77e39ce6e67
Chester Lau <chesterlau@live.com>
2020-07-30 23:25:40 +1000

    Added a health controller for health checks

---
+++ src/ProductManagement.API/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ProductManagement.API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
